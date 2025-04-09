63e903915d6b74757db27e139f992d76194c5a6f
luis-susunaga <susunaga.corp@gmail.com>
2025-04-09 11:55:12 -0600

    Create arithmeticOperators.py

---
+++ arithmeticOperators.py
@@ -0,0 +1,88 @@
+#this is an addition
+print (5 + 10)
+a=5
+b=10
+c=a+b
+print (c)
+
+#write your own addition examples (2)
+
+
+
+
+
+
+
+#this is a substraction
+print(29-23)
+d=50
+e=30
+f=d-e
+print(f)
+
+#write your own substraction examples (2)
+
+
+
+
+
+#this is a multiplication
+print(2*3)
+g=10
+h=3
+i=g*h
+print(i)
+
+#your own multiplication examples (2)
+
+
+
+
+
+
+
+#this is a division
+print(10/2)
+div1=15
+div2=3
+divResult=div1/div2
+print(divResult)
+
+#your own division examples (2)
+
+
+#this is a modulus
+print(4%3)
+m=15
+n=4
+o=m%n
+print(o)
+
+#your own modulus examples (2)
+
+
+
+
+
+
+#this is an exponent
+print(8**2)
+p=3
+q=4
+r=p**q
+print(r)
+
+#your own exponent examples (2)
+
+
+
+
+
+#this is a floor division
+print (7//2)
+s=18
+t=4
+u=s//t
+print(u)
+
+#your own floor division examples (2)
