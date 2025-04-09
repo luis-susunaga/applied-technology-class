482139d44c614ccfc98ee399660a8338e560a35f
luis-susunaga <susunaga.corp@gmail.com>
2025-04-09 11:58:16 -0600

    Create simplyData.py

---
+++ simplyData.py
@@ -0,0 +1,43 @@
+#this is a string
+"Hello World"
+"My name is Luis"
+"I really like Soccer"
+
+#write your own string examples (3)
+
+
+
+
+#this is a number
+2025
+15
+300
+
+#write your own number examples (3)
+
+
+
+
+#this is a boolean
+
+True
+False
+
+#in this case there are just only two values for boolean data
+
+#this is an integer
+
+10
+1000
+380
+
+#write your own integer examples (3)
+
+
+
+#this is a float
+28.9
+5.5
+40.6
+
+#write your own float examples (3)
