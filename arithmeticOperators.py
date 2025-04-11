1dbdebc8aeeec5068f8a466c3eea5f3d11a0110b
luis-susunaga <susunaga.corp@gmail.com>
2025-04-11 09:44:47 -0600

    Update arithmeticOperators.py

--- arithmeticOperators.py
+++ arithmeticOperators.py
@@ -1,8 +1,8 @@
 #this is an addition
 print (5 + 10)
-a=5
-b=10
-c=a+b
+sumValue1=5
+sumValue2=10
+sumResult=a+b
 print (c)
 
 #write your own addition examples (2)
@@ -15,9 +15,9 @@ print (c)
 
 #this is a substraction
 print(29-23)
-d=50
-e=30
-f=d-e
+subValue1=50
+subValue2=30
+subResult=d-e
 print(f)
 
 #write your own substraction examples (2)
@@ -28,9 +28,9 @@ print(f)
 
 #this is a multiplication
 print(2*3)
-g=10
-h=3
-i=g*h
+multValue1=10
+multValue2=3
+multResult=g*h
 print(i)
 
 #your own multiplication examples (2)
@@ -43,8 +43,8 @@ print(i)
 
 #this is a division
 print(10/2)
-div1=15
-div2=3
+divValue1=15
+divValue2=3
 divResult=div1/div2
 print(divResult)
 
@@ -53,10 +53,12 @@ print(divResult)
 
 #this is a modulus
 print(4%3)
-m=15
-n=4
-o=m%n
+modValue1=15
+modValue2=4
+modResult=m%n
 print(o)
+
+#the modulus operator returns the remainder after dividing one number by another.
 
 #your own modulus examples (2)
 
@@ -67,9 +69,9 @@ print(o)
 
 #this is an exponent
 print(8**2)
-p=3
-q=4
-r=p**q
+expValue1=3
+expValue2=4
+expResult=p**q
 print(r)
 
 #your own exponent examples (2)
@@ -80,9 +82,11 @@ print(r)
 
 #this is a floor division
 print (7//2)
-s=18
-t=4
-u=s//t
+floValue1=18
+floValue2=4
+floResult=s//t
 print(u)
+
+#the floor division operator returns the whole number (integer) result of a division, ignoring the remainder.
 
 #your own floor division examples (2)
